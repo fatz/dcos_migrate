494a9b496c83b804eb933d006a17c630db82c0c9
Jan Ulferts <julferts@d2iq.com>
2020-12-28 15:23:20 +0100

    improved dependency test

--- tests/test_plugin_manager.py
+++ tests/test_plugin_manager.py
@@ -1,7 +1,4 @@
 from dcos_migrate.plugins.plugin_manager import PluginManager
-from dcos_migrate.plugins.cluster import ClusterPlugin
-from dcos_migrate.plugins.secret import SecretPlugin
-from dcos_migrate.plugins.marathon import MarathonPlugin
 from dcos_migrate.plugins.plugin import MigratePlugin
 
 
@@ -13,36 +10,30 @@ def test_auto_discovery():
     assert "secret" in pm.plugins.keys()
 
 
-class Test1Plugin(MigratePlugin):
-    """docstring for ClusterPlugin."""
-    plugin_name = "test1"
-    # No depends wanna run first
-
-    def __init__(self):
-        super(Test1Plugin, self).__init__()
-
-
-class Test2Plugin(MigratePlugin):
-    """docstring for ClusterPlugin."""
-    plugin_name = "test2"
-    migrate_depends = ['test1']
-
-    def __init__(self):
-        super(Test1Plugin, self).__init__()
-
-
-class Test3Plugin(MigratePlugin):
-    """docstring for ClusterPlugin."""
-    plugin_name = "test3"
-    migrate_depends = ['test1', 'test2']
-
-    def __init__(self):
-        super(Test1Plugin, self).__init__()
-
-# @pytest.mark.xfail(reason="Dependency management not fully working")
-
-
 def test_dependencies():
+    class Test1Plugin(MigratePlugin):
+        """docstring for ClusterPlugin."""
+        plugin_name = "test1"
+        # No depends wanna run first
+
+        def __init__(self):
+            super(Test1Plugin, self).__init__()
+
+    class Test2Plugin(MigratePlugin):
+        """docstring for ClusterPlugin."""
+        plugin_name = "test2"
+        migrate_depends = ['test1']
+
+        def __init__(self):
+            super(Test1Plugin, self).__init__()
+
+    class Test3Plugin(MigratePlugin):
+        """docstring for ClusterPlugin."""
+        plugin_name = "test3"
+        migrate_depends = ['test1', 'test2']
+
+        def __init__(self):
+            super(Test1Plugin, self).__init__()
 
     pm = PluginManager(plugins={
         'test1': Test1Plugin,
@@ -52,5 +43,10 @@ def test_dependencies():
 
     assert len(pm.plugins) == 3
     assert len(pm.migrate) == 3
+
+    assert pm.migrate_batch == [[pm.plugins['test1']],
+                                [pm.plugins['test2']],
+                                [pm.plugins['test3']]
+                                ]
 
     # assert pm.migrate_batch is None
