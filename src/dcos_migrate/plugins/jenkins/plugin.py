1fc707b0cb52d21328dba7dee548dc1ad042b574
Jan Ulferts <julferts@d2iq.com>
2020-12-29 17:02:47 +0100

    add initial jenkins plugin

--- src/dcos_migrate/plugins/jenkins/plugin.py
+++ src/dcos_migrate/plugins/jenkins/plugin.py
@@ -1,6 +1,8 @@
 from dcos_migrate.plugins.plugin import MigratePlugin
 from dcos_migrate.plugins.marathon import MarathonPlugin
 from dcos_migrate.system import DCOSClient, BackupList, Backup
+from base64 import b64decode
+import json
 
 # we could have an abstract for cosmos services as we can extract package options in the same way
 
@@ -15,6 +17,16 @@ class JenkinsPlugin(MigratePlugin):
         super(JenkinsPlugin, self).__init__()
 
     def backup(self, client: DCOSClient, backupList: BackupList, **kwargs) -> BackupList:
-        bl = backupList
-        # extract PACKAGE options from marathon backup
-        pass
+        bl = BackupList()
+        for b in backupList.backups(pluginName="marathon"):
+            if b.data and 'labels' in b.data and "DCOS_PACKAGE_NAME" in b.data['labels'] and b.data['labels']['DCOS_PACKAGE_NAME'] == "jenkins":
+                # we found a jenkins package lets extract the config
+                if 'DCOS_PACKAGE_OPTIONS' in b.data['labels']:
+                    options_str = b64decode(
+                        b.data['labels']['DCOS_PACKAGE_OPTIONS'])
+
+                    options = json.loads(options_str)
+
+                    bl.append(Backup(pluginName=self.plugin_name, backupName=Backup.renderBackupName(
+                        b.data['labels']['DCOS_SERVICE_NAME'])))
+        return bl
--- src/dcos_migrate/system/backup_list.py
+++ src/dcos_migrate/system/backup_list.py
@@ -1,6 +1,6 @@
-import os
 from .storable_list import StorableList
 from .backup import Backup
+from jsonpath_ng import parse
 
 
 class BackupList(StorableList):
@@ -10,11 +10,29 @@ class BackupList(StorableList):
         super(BackupList, self).__init__(path)
         self._dry = dry
 
-    def backups(self, pluginName):
+    def backups(self, pluginName: str):
+        newList = BackupList()
+
+        for b in self:
+            if b.plugin_name == pluginName:
+                newList.append(b)
+        return newList
+
+    def backup(self, pluginName: str, backupName: str):
         pass
 
-    def backup(self, pluginName, backupName):
-        pass
+    def match_jsonpath(self, jsonPath):
+        bl = BackupList()
+        jsonpath_expr = parse(jsonPath)
+
+        for b in self:
+            # this is quite stupid but related to the Backup object structure
+            # maybe there is a better way to make .data directly part of the list
+            res = jsonpath_expr.find([b.data])
+            if res and len(res) > 0:
+                bl.append(b)
+
+        return bl
 
     def append_data(self, pluginName: str, backupName: str,
                     extenstion: str, data: str):
